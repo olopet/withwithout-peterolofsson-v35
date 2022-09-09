230a8904f1e0f0bd362907369bcf51252ee3d647
Peter <petolofsson@gmail.com>
2022-09-09 14:39:15 +0200

    3C Klar. Samt jag förstår allt. :)

--- web/3c/index.php
+++ web/3c/index.php
@@ -5,58 +5,77 @@
 
 <?php
 
-$cars = array(
-    array(
-        "Top 10 Position: " => "1",
-        "Model" => "Ford F-Series",
-    ),
-    array(
-        "Top 10 Position: " => "2",
-        "Model" => "Chevrolet Silverado",
-    ),
-    array(
-        "Top 10 Position: " => "3",
-        "Model" => "Ram Pickup",
-    ),
-    array(
-        "Top 10 Position: " => "4",
-        "Model" => "Toyota RAV4",
-    ),
-    array(
-        "Top 10 Position: " => "5",
-        "Model" => "Honda CR-V",
-    ),
-    array(
-        "Top 10 Position: " => "6",
-        "Model" => "Toyota Camry",
-    ),
-    array(
-        "Top 10 Position: " => "7",
-        "Model" => "Chevrolet Equinox",
-    ),
-    array(
-        "Top 10 Position: " => "8",
-        "Model" => "Honda Civic",
-    ),
-    array(
-        "Top 10 Position: " => "9",
-        "Model" => "GMC Sierra",
-    ),
-    array(
-        "Top 10 Position: " => "10",
-        "Model" => "Toyota Tacoma",
-    )
-);
+//Lista på 10 frukter jag gillar :9
+$fruits = [
+    ['title' => 'Apple'],
+    ['title' => 'Banana'],
+    ['title' => 'Lime'],
+    ['title' => 'Pear'],
+    ['title' => 'Orange'],
+    ['title' => 'Blueberry'],
+    ['title' => 'Mango'],
+    ['title' => 'Avocado'],
+    ['title' => 'Pineapple'],
+    ['title' => 'Strawberry'],
+    ];
 
-    if(isset($_GET['cars']))
-    {
-        echo $_GET['cars'];
+
+    if (isset ($_GET['page']) && !empty($_GET['page']))
+        {
+        // Sidan att visa (kommer via URL parameter)
+        $page = intval($_GET['page']);
+
+        } else {
+
+        // Om inga parametrar finsn går den automatiskt till första sidan
+        header('location:/3C/index.php?page=1');
+        
+        }
+
+    // Antalet objekt från array som ska vias
+    $pageSize = 2;
+
+    // Räknar antalet objekt i array $Fruits och kalkylerar antalet sidor (avrundat uppåt med Ceil)
+    $totalRecords = count($fruits);
+    $totalPages   = ceil($totalRecords / $pageSize);
+
+    // Validering att inkommen $_GET 'page' inte får vara större än antalet nummer totala sidor.
+    if ($page > $totalPages) {
+        $page = $totalPages;
     }
-    else {
-        echo 'no url parameters';
-    } 
-    
-?>
+
+    // Validering att $_GET 'page' inte kan vara mindre än ett.
+    if ($page < 1) {
+        $page = 1;
+    }
+
+     // Kalkylerar positionen för den första datapunkten i arrayen som sidan ska visa
+    $offset = ($page - 1) * $pageSize;
+    //echo $offset;
+
+    // Hämtar de objekt som ska visas från array $Fruits
+    $fruits = array_slice($fruits, $offset, $pageSize);
+
+    //Printar ut två values med hjälp av Key 'title' från array $Fruits med hjälp av foreach.
+    foreach ($fruits as $fruit) { 
+
+        echo $fruit['title'] . " "; 
+
+         } 
+
+    // Variabler för paginerings länkar.
+    $pageFirst = $page > 1 ? 1 : '';
+    $pagePrev  = $page > 1 ? $page-1 : '';
+    $pageNext  = $page < $totalPages ? $page + 1 : '';
+    $pageLast  = $page < $totalPages ? $totalPages : '';
+
+    ?>
+
+    <br><br>
+    <a href="index.php?page=<?php echo $pageFirst; ?>">« First</a>
+    <a href="index.php?page=<?php echo $pagePrev; ?>">Prev</a>
+    <a href="index.php?page=<?php echo $pageNext; ?>">Next</a>
+    <a href="index.php?page=<?php echo $pageLast; ?>">Last »</a>
 
 </body>
 </html>
